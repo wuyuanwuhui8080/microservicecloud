2d8ed89b95258cb93343777c53bf7a51b4d94f4c
wuyuanwuhui8080 <chixinyishui1578@163.com>
2019-09-10 17:42:06 +0800

    ""

--- microservicecloud-provider-dept-8001/src/main/java/com/nzh/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/nzh/springcloud/DeptProvider8001_App.java
@@ -2,9 +2,10 @@ package com.nzh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient  //注册进eureka服务端
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
